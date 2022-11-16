240a4aa31b9b921262d6aa2484cb8599dbc58c9a
Nasar Issa <44656132+itsaissa@users.noreply.github.com>
2022-11-16 12:23:54 -0800

    Style and add functionality to button

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     let password = document.querySelector("#password");
-    let confirmPassowrd = document.querySelector("#confirmPassword");
+    let confirmPassword = document.querySelector("#confirmPassword");
 
     let error = document.querySelector("#error");
 
-    if(password.value != confirmPassowrd.value){
-        error.textContent = "Passwords do not match."
+    if(password.value == "" && confirmPassword.value == "") 
+        error.textContent = "*Passwords do not match"
+    else if (password.value === confirmPassowrd.value){
+        error.textContent = ""
     }
 });
